5615b0c8b2ddcdeb1983572dc9271254d01b2318
Rutger van Haasteren <vhaasteren@gmail.com>
2015-09-18 13:11:44 -0700

    Made the combination script accept the 4 movies as parameters

--- mcmcmovies/combine-videos.sh
+++ mcmcmovies/combine-videos.sh
@@ -2,4 +2,4 @@
 # Combine 4 MCMC videos, as outputted by the mcmcmovie.py script. Make sure the
 # sample times for all four of the MCMC runs are the same for fair comparison.
 
-ffmpeg -i movie1.avi -i movie2.avi -i movie3.avi -i movie4.avi -filter_complex "[0:0]pad=iw*2:ih*2[a];[a][1:0]overlay=w[b];[b][2:0]overlay=0:h[c];[c][3:0]overlay=w:h" -shortest combinedmovie.avi
+ffmpeg -i $1 -i $2 -i $3 -i $4 -filter_complex "[0:0]pad=iw*2:ih*2[a];[a][1:0]overlay=w[b];[b][2:0]overlay=0:h[c];[c][3:0]overlay=w:h" -shortest combinedmovie.avi
