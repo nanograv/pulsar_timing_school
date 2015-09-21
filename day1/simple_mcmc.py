c7c472ed6ae502869e1cd289201db8940372e197
Justin Ellis <justin.ellis18@gmail.com>
2015-09-21 02:37:14 -0700

    Finished day1 exercises

--- day1/simple_mcmc.py
+++ day1/simple_mcmc.py
@@ -30,17 +30,17 @@ class SimpleMCMC(object):
         
         """
         
-        ndim = len(p0)
+        self.ndim = len(p0)
         naccepted = 0
         
         # initialize arrays
-        self.chain = np.zeros((N, ndim))
+        self.chain = np.zeros((N, self.ndim))
         self.lnlike, self.lnprob = np.zeros(N), np.zeros(N)
         
         # get likelihood and prior for initial point
         lnlike0, lnprior0 = self.lnlikefun(p0), self.lnpriorfn(p0)
         
-        it = 0
+        self.iter = 0
         # start iterations
         for ii in range(N):
             
@@ -67,12 +67,12 @@ class SimpleMCMC(object):
             self.lnlike[ii], self.lnprob[ii] = lnlike0, (lnlike0 + lnprior0)
 
             
-            if it % 100 == 0 and it > 0:
+            if self.iter % 100 == 0 and self.iter > 0:
                 sys.stdout.write('\r')
-                sys.stdout.write('Finished %g percent. Acceptance Rate: %g'%(it / N * 100, naccepted / it * 100))
+                sys.stdout.write('Finished %g percent. Acceptance Rate: %g'%(self.iter / N * 100, naccepted / self.iter * 100))
                 sys.stdout.flush()
             
-            it += 1
+            self.iter += 1
             
     
     def jump(self, p0):
@@ -87,12 +87,42 @@ class SimpleMCMC(object):
         
         qxy = 0
         q = p0.copy()
-
-        # step sizes
-        probs = [0.05, 0.7, 0.25]
-        sizes = [0.1, 1.0, 10.0]
-        scale = np.random.choice(sizes, p=probs)
         
-        q += np.random.randn(len(q)) * self.sigmas * scale
+        # 50/50 jup probability
+        jumps = ['gaussian', 'de']
+        jprobs = [0.5, 0.5]
+
+        # draw jump type
+        jname = np.random.choice(jumps, p=jprobs)
+        
+        # differential evolution
+        if jname == 'de' and self.iter > 1000:
+
+            # draw a random integer from 0 - iter
+            mm = np.random.randint(0, self.iter)
+            nn = np.random.randint(0, self.iter)
+
+            # make sure mm and nn are not the same iteration
+            while mm == nn:
+                nn = np.random.randint(0, self.iter)
+
+            # mode jump
+            if np.random.rand() > 0.5:
+                scale = 1.0
+
+            else:
+                scale = 2.4 / np.sqrt(2 * self.ndim)
+
+            q += scale * (self.chain[mm,:] - self.chain[nn,:])
+
+        # standard gaussian default
+        else:
+            # step sizes
+            probs = [0.05, 0.7, 0.25]
+            sizes = [0.1, 1.0, 10.0]
+            scale = np.random.choice(sizes, p=probs)
+            q += np.random.randn(len(q)) * self.sigmas * scale
+
+
         
         return q, qxy
